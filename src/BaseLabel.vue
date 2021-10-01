87436bbb1e6fb7d1dd08109e8b96511166d45c4f
nandolaorden <57456050+nandolaorden@users.noreply.github.com>
2021-10-01 12:13:25 +0200

    add label component (#6)

---
+++ src/BaseLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <label class="block font-medium text-sm text-gray-700">
+    <span v-if="value">{{ value }}</span>
+    <span v-else><slot></slot></span>
+    <span v-if="mandatory" class="text-red-600">*</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    mandatory: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      required: false
+    },
+  }
+}
+</script>
